e00a56bb58e6bb5b9c9c111fa9e0dea5ae470d13
Darynazar <63154066+Darynazar@users.noreply.github.com>
2020-04-25 14:32:20 +0200

    Update Core.php

--- mvcframework/app/libraries/Core.php
+++ mvcframework/app/libraries/Core.php
@@ -1,47 +1,56 @@
 <?php
-    //Core App Class
-    class Core {
-        protected $currentController = 'Pages';
-        protected $currentMethod = 'index';
-        protected $params = [];
+  /*
+   * App Core Class
+   * Creates URL & loads core controller
+   * URL FORMAT - /controller/method/params
+   */
+  class Core {
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
+    protected $params = [];
 
-        public function __construct() {
-            $url = $this->getUrl();
+    public function __construct(){
+      //print_r($this->getUrl());
 
-            //Look in 'controllers' for first value, ucwords will capitalize first letter
-            if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
-                // Will set a new controller
-                $this->currenController = ucwords($url[0]);
-                unset($url[0]);
-            }
+      $url = $this->getUrl();
 
-            //Require the controller
-            require_once '../app/controllers/' . $this->currentController . '.php';
-            $this->currentController = new $this->currentController;
+      // Look in BLL for first value
+      if(file_exists('../app/BLL/' . ucwords($url[0]). '.php')){
+        // If exists, set as controller
+        $this->currentController = ucwords($url[0]);
+        // Unset 0 Index
+        unset($url[0]);
+      }
 
-            //Check for second part of the URL
-            if (isset($url[1])) {
-                if (method_exists($this->currentController, $url[1])) {
-                    $this->currentMethod = $url[1];
-                    unset($url[1]);
-                }
-            }
+      // Require the controller
+      require_once '../app/BLL/'. $this->currentController . '.php';
 
-            //Get parameters
-            $this->params = $url ? array_values($url) : [];
+      // Instantiate controller class
+      $this->currentController = new $this->currentController;
 
-            //Call a callback with array of params
-            call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
+      // Check for second part of url
+      if(isset($url[1])){
+        // Check to see if method exists in controller
+        if(method_exists($this->currentController, $url[1])){
+          $this->currentMethod = $url[1];
+          // Unset 1 index
+          unset($url[1]);
         }
+      }
 
-        public function getUrl() {
-            if(isset($_GET['url'])) {
-                $url = rtrim($_GET['url'], '/');
-                // Aloows you to filter variables as string/number
-                $url = filter_var($url, FILTER_SANITIZE_URL);
-                //Breaking it into an array
-                $url = explode('/', $url);
-                return $url;
-            }
-        }
+      // Get params
+      $this->params = $url ? array_values($url) : [];
+
+      // Call a callback with array of params
+      call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
+
+    public function getUrl(){
+      if(isset($_GET['url'])){
+        $url = rtrim($_GET['url'], '/');
+        $url = filter_var($url, FILTER_SANITIZE_URL);
+        $url = explode('/', $url);
+        return $url;
+      }
+    }
+  }
