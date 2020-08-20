4a76dd9de514b26d79f11bfb9b5d44df327e4e28
Darynazar <63154066+Darynazar@users.noreply.github.com>
2020-08-20 22:15:45 +0200

    Update config.php

--- mvcframework/app/config/config.php
+++ mvcframework/app/config/config.php
@@ -3,13 +3,13 @@
     define('DB_HOST', 'localhost'); //Add your db host
     define('DB_USER', 'root'); // Add your DB root
     define('DB_PASS', ''); //Add your DB pass
-    define('DB_NAME', ''); //Add your DB Name
+    define('DB_NAME', 'mvcframework'); //Add your DB Name
 
     //APPROOT
     define('APPROOT', dirname(dirname(__FILE__)));
 
     //URLROOT (Dynamic links)
-    define('URLROOT', 'localhost/mvcframework');
+    define('URLROOT', 'http://localhost/mvcframework');
 
     //Sitename
     define('SITENAME', 'MVC Framework');
