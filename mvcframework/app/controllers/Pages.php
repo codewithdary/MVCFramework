967a73c2f9397ee3d97fcc3f83ebbebab613c0e2
Darynazar <63154066+Darynazar@users.noreply.github.com>
2020-08-20 22:16:08 +0200

    Update Pages.php

--- mvcframework/app/controllers/Pages.php
+++ mvcframework/app/controllers/Pages.php
@@ -1,7 +1,7 @@
 <?php
 class Pages extends Controller {
     public function __construct() {
-        $this->userModel = $this->model('User');
+        //$this->userModel = $this->model('User');
     }
 
     public function index() {
