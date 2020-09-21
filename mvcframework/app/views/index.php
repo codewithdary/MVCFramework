0ee543ea08c7ec27e0f8bfc38d5d219a0f02025a
Darynazar <63154066+Darynazar@users.noreply.github.com>
2020-09-21 12:15:02 +0200

    Create index.php

---
+++ mvcframework/app/views/index.php
@@ -0,0 +1,9 @@
+<?php
+/* This is how you echo out database information on the screen
+foreach ($data['users'] as $user) {
+    echo "Information: " . $user->user_name . $user->user_email;
+    echo "<br>";
+}
+*/
+?>
+HOME PAGE
